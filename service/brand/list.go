335fe0413199b2750b636c8db99e13fc76a95224
DumbCaveSpider <56347227+DumbCaveSpider@users.noreply.github.com>
2025-12-11 16:12:03 +0800

    tweak stuff

--- service/brand/list.go
+++ service/brand/list.go
@@ -21,28 +21,33 @@ func init() {
 		if r.Method == http.MethodGet {
 			header.Set("Content-Type", "application/json")
 
+			// require login
 			uid, err := access.GetSessionUserID(r)
 			if err != nil {
 				http.Error(w, "Unauthorized", http.StatusUnauthorized)
 				return
 			}
 
-			allImgs, err := database.ListAllImages()
+			// Get all images
+			imgList, err := database.ListAllImages()
 			if err != nil {
 				log.Error("Failed to list images: %s", err.Error())
 				http.Error(w, "Failed to list images", http.StatusInternalServerError)
 				return
 			}
 
-			userImgs, err := database.FilterImagesByUser(allImgs, uid)
+			// Filter by user
+			userImages, err := database.FilterImagesByUser(imgList, uid)
 			if err != nil {
-				log.Error("Failed to filter images: %s", err.Error())
+				log.Error("Failed to filter images for user %d: %s", uid, err.Error())
 				http.Error(w, "Failed to filter images", http.StatusInternalServerError)
 				return
 			}
 
+			log.Debug("Returning %d images for user %d", len(userImages), uid)
+
 			w.WriteHeader(http.StatusOK)
-			if err := json.NewEncoder(w).Encode(userImgs); err != nil {
+			if err := json.NewEncoder(w).Encode(userImages); err != nil {
 				log.Error("Failed to encode response: %s", err.Error())
 				http.Error(w, "Failed to encode response", http.StatusInternalServerError)
 				return
